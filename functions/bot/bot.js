acb32e457f9f0e423eb91a06fc9c7ef4b1163bf4
MasterOfTheChain <mickh1992@gmail.com>
2022-10-06 19:46:08 +0700

    Update Bot with WebApp

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -1,10 +1,16 @@
 const { Telegraf } = require("telegraf")
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+const web_link = "https://cheery-platypus-094af3.netlify.app/";
+
 bot.start(ctx => {
   console.log("Received /start command")
   try {
-    return ctx.reply("Hi")
+    return ctx.reply("Welcome", {
+        reply_markup: {
+          keyboard: [[{ text: "Open App", web_app: { url: web_link } }]],
+        },
+      })
   } catch (e) {
     console.error("error in start action:", e)
     return ctx.reply("Error occured")
